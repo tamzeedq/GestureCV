6aa8c31e5117029e17ae1c6d1b9fc46d5a357cff
tamzeedq <74509993+tamzeedq@users.noreply.github.com>
2024-02-25 11:20:08 -0800

    changed mouse smoothing logic

--- mouse.py
+++ mouse.py
@@ -3,15 +3,17 @@ import numpy as np
 import math
 
 class Mouse:
-    def __init__(self, duration=0.3, cam_dim=(640, 480), screen_dim=(1920, 1080)):
+    def __init__(self, smth_factor=0.3, duration=0.3, cam_dim=(640, 480), screen_dim=(1920, 1080)):
         """
         Mouse object to handle mouse movement and events.
 
         Args:
+            smth_factor (float, optional): Smoothing factor for mouse movement. Defaults to 0.3.
             duration (float, optional): Duration to complete a mouse move. Defaults to 0.5.
-            cam_dim (tuple[int], optional): Dimemsions of camera. Defaults to (640, 480).
-            screen_dim (tuple[int], optional): Dimensions of monitor. Defaults to (1920, 1080).
+            cam_dim (tuple[int]): Dimemsions of camera. Defaults to (640, 480).
+            screen_dim (tuple[int]): Dimensions of monitor. Defaults to (1920, 1080).
         """
+        self.smth_factor = smth_factor
         self.duration = duration
         self.cam_w, self.cam_h = cam_dim
         self.screen_w, self.screen_h = screen_dim
@@ -34,7 +36,14 @@ class Mouse:
         x = int(np.interp(lmx*self.cam_w, [0, self.cam_w], [0, self.screen_w]))
         y = int(np.interp(lmy*self.cam_h, [0, self.cam_h], [0, self.screen_h]))
         
-        pg.moveTo(self.screen_w - x, y, self.duration, pg.easeOutQuad)
+        new_mx = int(self.prev_mx + self.smth_factor * (x - self.prev_mx))
+        new_my = int(self.prev_my + self.smth_factor * (y - self.prev_my))
+        
+        # different smoothing method but not as smooth, adding duration feels laggy
+        # pg.moveTo(self.screen_w - x, y, self.duration, pg.easeOutQuad)
+        
+        pg.moveTo(self.screen_w - new_mx, new_my)
+        prev_mx, prev_my = new_mx, new_my
     
     
     def click(self, detection_result):
